475c97e868d163c4437bf70a8088d9d20ccc8b79
kumar-t-chavan <kumartchavan1552@gmail.com>
2025-10-14 15:14:00 +0530

    media Queries add

--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom' // ✅ import Link
 
+
+
 const Navbar = () => {
   return (
     <div>
@@ -15,9 +17,9 @@ const Navbar = () => {
             <li><Link className='Link' to="/">Home</Link></li>
             <li><Link className='Link' to="/about">About</Link></li>
             <li><Link className='Link' to="/skills">Skills</Link></li>
-            <li><Link className='Link' to="/participate">Participate</Link></li>
-            <li><Link className='Link' to="/achievement">Achievement</Link></li>
-            <li className='contact'><Link className='Link' to="/contact">Contact</Link></li>
+            <li><Link className='Link' to="/participate">Projects</Link></li>
+            <li className='xyz'><Link className='Link' to="/achievement">Achievement</Link></li>
+            <li className='xyz'><Link className='Link' to="/contact" style={{backgroundColor: "#381409",padding: "8px",borderRadius:"3px",paddingLeft:"40px",paddingRight:"40px"}}>Contact</Link></li>
           </ul>
         </nav>
       </div>
